016c7403c47a2e8cb8bbe83141fb4bf3a4f76118
MrBurp <andreass.ziegler@gmail.com>
2019-10-25 20:05:38 +0200

    half bug fixed

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-function openTab(e)
+// window.alert("works");
+
+function openTab() {
+  let element = document.querySelector(".extern");
+  if ((element.style.visibility = "hidden")) {
+    // element.style.display = "block";
+    element.style.visibility = "visible";
+  } else {
+    // element.style.display = "none";
+    element.style.visibility = "hidden";
+  }
+  console.log(openTab());
+}
