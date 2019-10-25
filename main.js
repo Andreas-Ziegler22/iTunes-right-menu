b75e34f6b08f9706e111a3afe3d3ff9369c6ee63
MrBurp <andreass.ziegler@gmail.com>
2019-10-25 21:00:38 +0200

    its near what should be

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 // window.alert("works");
 
+//why does works
+
 function openTab() {
   let element = document.querySelector(".extern");
   if ((element.style.visibility = "hidden")) {
-    // element.style.display = "block";
     element.style.visibility = "visible";
   } else {
-    // element.style.display = "none";
     element.style.visibility = "hidden";
   }
-  console.log(openTab());
+  console.log("element");
 }
+
+// that 2 functions works but I would like to the tab be fixed and hovered
+
+// function openTab() {
+//   document.querySelector(".extern").style.visibility = "visible";
+// }
+// function closeTab() {
+//   document.querySelector(".extern").style.visibility = "hidden";
+// }
